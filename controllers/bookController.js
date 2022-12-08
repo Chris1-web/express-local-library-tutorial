d8647ed4c798c8d24bb0fd067472e64cbbfa7c42
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-12-08 13:52:05 +0100

    create view form homepage on catalog route

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,5 +1,5 @@
 const Book = require("../models/book");
-const Author = require("../models/Author");
+const Author = require("../models/author");
 const Genre = require("../models/genre");
 const BookInstance = require("../models/bookinstance");
 
