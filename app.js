f4d03da0d9f81935e8023cd63f97f0b257404e0f
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-12-08 03:04:38 +0100

    update app.js to have catalog rout

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var catalogRouter = require("./routes/catalog");
 const wiki = require("./routes/wiki");
 
 var app = express();
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter);
 app.use("/wiki", wiki);
 
 // catch 404 and forward to error handler
